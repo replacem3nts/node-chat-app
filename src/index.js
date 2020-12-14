e898a1d8f6dde22e7e809641ae77aedd39f82569
replacem3nts <donny.landis@gmail.com>
2020-12-14 15:21:55 -0500

    sets up basic scaffolding

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
 
 app.listen(port, () => {
   console.log(`Server is up on port: ${port}`);
